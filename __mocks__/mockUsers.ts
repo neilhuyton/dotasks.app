488ffa7fab310a6f4e7ce5db4a406123c96720cf
Neil Huyton <github@nehu.me>
2026-03-01 17:40:11 +0000

    chore: mock user update

--- __mocks__/mockUsers.ts
+++ __mocks__/mockUsers.ts
@@ -42,7 +42,7 @@ export const mockUsers: MockUser[] = [
   },
   {
     id: "27e72eb9-a0ad-4714-bd7a-c148ac1b903e",
-    email: "neil.huyton@gmail.com",
+    email: "cool@example.com",
     password: "$2b$10$jEeurRBvzMKgzXNYqihFiedyVyMCJDlC293i/MYoY9IPVrWeZOhX",
     verificationToken: TEST_VERIFICATION_TOKENS.DELAYED_SUCCESS,
     isEmailVerified: false,
@@ -53,7 +53,7 @@ export const mockUsers: MockUser[] = [
   },
   {
     id: "fb208768-1bf8-4f8d-bcad-1f94c882ed93",
-    email: "hi@neilhuyton.com",
+    email: "elvis@example.com",
     password: "$2b$10$RBmt.5/HTA/qk5Y47NYgvuZ5TA0AurgAUy0vDeytiUKsvZUeR.lrG",
     verificationToken: null,
     isEmailVerified: true,
