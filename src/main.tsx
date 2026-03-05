41e3bb22c884f75f8d9123b8ff50c0905b3032e4
Neil Huyton <github@nehu.me>
2026-03-05 14:22:21 +0000

    chore/rt-test4

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 // src/main.tsx
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { Root } from "./app/Root";
@@ -10,11 +9,7 @@ import "./index.css";
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 
-createRoot(rootElement).render(
-  <StrictMode>
-    <Root />
-  </StrictMode>,
-);
+createRoot(rootElement).render(<Root />);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
